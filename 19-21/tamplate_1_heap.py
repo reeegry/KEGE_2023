af74e26cb11986f36dbb7a2bdafe77c528f09529
reeegry <6u8654u4@gmail.com>
2023-05-12 16:44:04 +0300

    19-21

--- 19-21/tamplate_1_heap.py
+++ 19-21/tamplate_1_heap.py
@@ -6,7 +6,7 @@ def game(stones, step, final_step):
     if stones >= (количество камней для победы): return step % 2 == final_step % 2
     if step == final_step: return 0
 
-    heaps = [game(stones + (условие 1)), game(stone * (условие 2)), (следующие услоия)]
+    heaps = [game(stones + (условие 1), step + 1, final_step), game(stone * (условие 2), step + 1, final_step), (следующие условия)]
     '''
     если следующий ход это ход игрок за которого мы ищем победу, то нам нужен ХОТЯ БЫ один выигрышный ход, иначе во всех ходах
     противника нужна победа(потому что у него нет стратегии)
@@ -18,3 +18,4 @@ for s in range(количество камней):
     for step in range(1, 5):
         if game(s, 0, step):
             print(s, step)
+            break
--- 19-21/tamplate_2_heaps.py
+++ 19-21/tamplate_2_heaps.py
@@ -1,9 +1,9 @@
-def game(stones1, stones2, , step, final_step):
+def game(stones1, stones2, step, final_step):
     if stones1 + stones2 >= (количество камней для победы): return step % 2 == final_step % 2
     if step == final_step: return 0
 
     heaps = [game(stones1 + (условие 1), stones2 , step + 1, final_step), game(stones1, stones2 + (условие 2), step + 1, final_step), 
-             game(stones1 * (условие 2), stones2,  step + 1, final_step), game(stones1, stones2 * (условие2), step + 1, final_step), (следующие услоия)]
+             game(stones1 * (условие 2), stones2,  step + 1, final_step), game(stones1, stones2 * (условие2), step + 1, final_step), (следующие условия)]
     return any(heaps) if (step  + 1) % 2 == final_step % 2 else all(heaps) #если нужна победа для неудачного хода, меняем all на any
 
 
@@ -11,3 +11,4 @@ for s in range(диапазон камней):
     for step in range(1, 5):
         if game(s,(начальное количество камней в 1 куче), 0, step):
             print(s, step)
+            break
